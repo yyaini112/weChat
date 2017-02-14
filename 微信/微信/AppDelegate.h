d7eae948e3fd74b0bc9959c5a32ce87f815e5433
user <user@userdemac.local>
2017-02-14 12:16:08 +0800

    初始化项目

---
+++ 微信/微信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微信
+//
+//  Created by user on 17/2/14.
+//  Copyright © 2017年 zichun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微信/微信/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  微信
+//
+//  Created by user on 17/2/14.
+//  Copyright © 2017年 zichun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
